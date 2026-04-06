188c096569b73fe4d71fc69c36f20cf3378515aa
tsengian36-rgb <tsengian36@gmail.com>
2026-04-06 16:07:09 -0400

    Update Ejercicio 5.py

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -14,3 +14,23 @@ def mostrar_menu_manipulacion():
       # Crear una nueva columna llamada "Poder Total"
       Poder_Total = datos["Ataque"] + datos["Defensa"] + datos["Velocidad"] + datos["PS"]
       datos["Poder_Total] = Poder_Total
+      print("\nDataFrame con nueva columna Poder Total:")
+      print(datos)
+      input("\nPresiona Enter para continuar...")
+      
+      elif opcion == "2":
+          # Ordena el DataFrame por "Poder Total" de mayor a menor
+          Orden_Poder_Total = datos.sort_values(by="Poder_Total", ascending=False)
+          print("\nPokémon ordenados por Poder Total de mayor a menor:")
+          print(Orden_Poder_Total)
+          input("\nPresiona Enter para continuar...")
+      
+    elif opcion == "0":
+          break
+      
+    else:
+      print("\Opciónno válida.")
+      input("Presiona Enter para continuar...")
+
+#Ejecutar menú
+mostrar_menu_manipulacion()

8753bd80c3b8847ff836c7219b9cf2cb1a28787d
tsengian36-rgb <tsengian36@gmail.com>
2026-04-06 13:18:03 -0400

    Import pandas and clean Pokémon data

--- pokedex.py
+++ pokedex.py
@@ -1,1 +1,7 @@
-
+import pandas as pd 
+from limpiar import limpiar_pokemones
+# Limpiar datos
+df_limpio = limpiar_pokemones("pokemon_primera_gen.csv", index=Flase)
+## 1. Lectura de datos
+datos = pd.read_csv("pokemones_primera_gen_limpio.csv")
+print(datos)

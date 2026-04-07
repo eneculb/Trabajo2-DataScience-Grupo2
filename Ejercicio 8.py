fbed0e401ac8a3d17b4bc0868cc7d97e418c4bc3
Natalia Torres Flores <natalia.tf20@gmail.com>
2026-04-07 15:43:41 -0400

    Update Ejercicio 8.py

--- Ejercicio 8.py
+++ Ejercicio 8.py
@@ -3,17 +3,18 @@ from Ejercicio_1 import datos
 import os 
 
 def mostrar_menupokemon():
-os.system("cls") 
-      print("1.\n Ver conclusiones del graficos")
-        print("2.\n Ver tipo Balanceado y mas especializado")
+    while True:
+        os.system("cls")
+          print("1.\n Ver conclusiones del graficos")
+          print("2.\n Ver tipo Balanceado y mas especializado")
           print("0.\n Salir")
         opcion = input("Elige una opción: ")
 if opcion == "1":
-            print("\nConclusiones sobre los Pokémon de la primera generación:")
-             print("- El tipo de rol del pokemon.")
-              print("  tipo Roca o Acero tienen muchisima defensa pero son los mas lentos.")
-               print("- Los pokemon de tipo Normal tienen los PS mas dispersos. Esto pasa por casos")
-                print("  extremos como Chansey, que tira el grafico para arriba por tener tanta vida.")
+           print("\nConclusiones sobre los Pokémon de la primera generación:")
+           print("- Algunos tipos como Roca tienen mucha defensa pero baja velocidad.")
+           print("- Los Pokémon de tipo Normal presentan gran variación en PS.")
+           print("- No hay una relación fuerte entre ataque y velocidad.")
+      
             input("\nPresiona Enter para continuar...")
 
         elif opcion == "2":
@@ -25,10 +26,10 @@ if opcion == "1":
             especializado = variabilidad.idxmax()
             
             print(f"\nEl tipo de Pokémon más BALANCEADO es: {balanceado}")
-             print("  -> Porque sus promedios de Ataque, Defensa, Velocidad y PS son super parecidos entre si.")
+            print("  -> Porque sus promedios de Ataque, Defensa, Velocidad y PS son super parecidos entre si.")
             
             print(f"\nEl tipo de Pokémon más ESPECIALIZADO es: {especializado}")
-             print("  -> Porque destaca demasiado en una estadistica pero sacrifica las otras (ej: mucha defensa y nada de velocidad).")
+            print("  -> Porque destaca demasiado en una estadistica pero sacrifica las otras (ej: mucha defensa y nada de velocidad).")
             
             input("\nPresiona Enter para continuar...")
 

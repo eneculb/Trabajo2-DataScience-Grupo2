bc0874d7b5f1a28a206504f6a9f44108108ef7d3
Benjamin <136281350+Benjamin43234@users.noreply.github.com>
2026-04-07 14:02:30 -0400

    Implement menu display for Pokémon options
    Added menu display functionality and improved code structure.

--- Ejercicio 8.py
+++ Ejercicio 8.py
@@ -1,3 +1,9 @@
 # 8. Ejercicios de interpretación
 from Ejercicio_1 import datos
-defdef mostrar_menupokemon():
+import os 
+def mostrar_menupokemon():
+os.system("cls") 
+      print("1./n Ver conclusiones del graficos")
+        print("2./n Ver tipo Balanceado y mas especializado")
+          print("0./n Salir")
+        opcion = input("Elige una opción: ")

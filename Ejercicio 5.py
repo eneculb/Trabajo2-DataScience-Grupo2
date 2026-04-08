60e5bdbfd22f4cf598312f5d85c99087d181b933
Elias Daniel Necul Briones <eneculb@gmail.com>
2026-04-08 00:44:14 -0400

    Update Ejercicio 5.py

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -1,36 +1,14 @@
-# 5. Manipulación de datos
-from Ejercicio_1 import datos
-import os
+#5. Manipulación de datos
+#-----------------------
+#- Crea una nueva columna llamada "Poder Total" que sea la suma de ataque, defensa, velocidad y PS.
+#- Ordena el DataFrame por "Poder Total" de mayor a menor.
 
-def mostrar_menu_manipulacion():
-  while True:
-    os.system("cls") # Limpiar pantalla en Windows
-    print("1. Crear columna Poder Total y mostrar primeros Pokémon")
-    print("2. Ordenar por Poder Total de mayor a menor ")
-    print("0. Salir")
-    opcion= input("Elige una opción: ")
-    
-    if opcion == "1":
-      # Crear una nueva columna llamada "Poder Total"
-      Poder_Total = datos["Ataque"] + datos["Defensa"] + datos["Velocidad"] + datos["PS"]
-      datos["Poder_Total] = Poder_Total
-      print("\nDataFrame con nueva columna Poder Total:")
-      print(datos)
-      input("\nPresiona Enter para continuar...")
-      
-      elif opcion == "2":
-          # Ordena el DataFrame por "Poder Total" de mayor a menor
-          Orden_Poder_Total = datos.sort_values(by="Poder_Total", ascending=False)
-          print("\nPokémon ordenados por Poder Total de mayor a menor:")
-          print(Orden_Poder_Total)
-          input("\nPresiona Enter para continuar...")
-      
-    elif opcion == "0":
-          break
-      
-    else:
-      print("\Opciónno válida.")
-      input("Presiona Enter para continuar...")
+import pandas as pd
 
-#Ejecutar menú
-mostrar_menu_manipulacion()
+df=pd.read_csv("pokemon_primera_gen.csv")
+
+df["Poder Total"]=df["Ataque"]+df["Defensa"]+df["Velocidad"]+df["PS"]
+
+df_ordenado=df.sort_values(by="Poder Total", ascending=False)
+
+print(df_ordenado)

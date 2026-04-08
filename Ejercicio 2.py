71ca0a5df7923eefd2aca490b812b454774e3f5b
Elias Daniel Necul Briones <eneculb@gmail.com>
2026-04-08 00:15:36 -0400

    Replace manual filtering with pandas DataFrame
    Refactor Pokémon filtering to use pandas for data manipulation.

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,32 +1,11 @@
-from Ejercicio_1 import datos
-import os
+#2. Filtrado y selección
+#-----------------------
+#- Filtra todos los Pokémon de tipo "Fuego".
+#- Selecciona solo las columnas Nombre, Tipo 1, Ataque y Velocidad.
 
-def mostrar_menu():
-  while True:
-    os.system("cls") # limpia la pantalla en Windows 
-    print("1. Mostrar Pokémones tipo Fuego")    
-    print("2. Mostrar columnas seleccionadas")
-    print("0. Salir")
-    opcion = input("Elegir una opción: ")
-    
-    if opcion == "1":
-      print("\nPokemones tipo FUEGO")
-      ### Filtrar todos los Pokémon de tipo "Fuego".
-     fuego= datos[datos["Tipo 1"]]= = "Fuego"
-      print(f"Cantidad de Pokémon tipo fuego: {len(fuego)}")
-      print(fuego)
-      ####
-      input("\nPresiona Enter para continuar...")
-    elif opcion == "2":
-      print("\nSolo las columnas Nombre, Tipo 1, Ataque y Velocidad")
-      ####Seleciona solo las columnas Nombre, Tipo 1, Ataque y Velocidad.
-      print(datos[["Nombre", "Tipo 1", "Ataque", "Velocidad"]])
-      ######
-      input("\nPresiona Enter para continuar...")
-    elif opcion == "0":
-      break
-    else:
-      print("\nOpción no valida.")
-      input("Presiona Enter para continuar...")
+import pandas as pd
 
-mostrar_menu()
+df=pd.read_csv("pokemon_primera_gen.csv")
+fuego=df[df["Tipo 1"]=="Fuego"]
+filtro=fuego[["Nombre","Tipo 1","Ataque","Velocidad"]]
+print(filtro)
